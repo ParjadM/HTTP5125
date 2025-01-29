ee0a4e5f10dbfe1dbd3d3e74f575e17a147b7505
Parjad Minooei <minooeip@gmail.com>
2025-01-29 16:03:30 -0500

    Initial commit for new project

---
+++ Question1/Controllers/Q1Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Question1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Q1Controller : ControllerBase
+    {
+        [HttpGet(template: "Welcome")]
+        public string Welcome()
+        {
+            return "Welcome to 5125!";
+        }
+    }
+}
