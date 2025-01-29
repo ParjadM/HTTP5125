96fa2b26918a1202ff04aa295815d4fb08762c5a
Parjad Minooei <minooeip@gmail.com>
2025-01-29 17:51:09 -0500

    Add files from Question7

---
+++ Question7/Controllers/q7Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Question7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class q7Controller : ControllerBase
+    {
+        [HttpGet(template: "timemachine")]
+        public string TimeMachine(int days)
+        {
+            DateTime adjustedDate = DateTime.Today.AddDays(days);
+            return adjustedDate.ToString("yyyy-MM-dd");
+
+        }
+    }
+}
