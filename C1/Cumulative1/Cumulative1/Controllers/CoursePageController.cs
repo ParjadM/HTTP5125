e29e5bd0c00baffd2aefe776968c00006d9e75c8
Parjad Minooei <minooeip@gmail.com>
2025-03-17 17:32:09 -0400

    Resolved merge conflicts

---
+++ C1/Cumulative1/Cumulative1/Controllers/CoursePageController.cs
@@ -0,0 +1,41 @@
+﻿using Cumulative1.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cumulative1.Controllers
+{
+    public class CoursePageController : Controller
+    {
+        
+        private readonly CourseAPIController _api;
+
+        // Constructor for CoursePageController
+        public CoursePageController(CourseAPIController api)
+        {
+            _api = api;
+        }
+
+        /// <summary>
+        /// Displays a list of Courses on a dynamic page.
+        /// </summary>
+        /// <returns>A view containing all Courses.</returns>
+        public IActionResult List()
+        {
+            
+            List<Course> Courses = _api.ListCourses();
+            return View("~/Views/Course/List.cshtml", Courses);
+        }
+
+
+        /// <summary>
+        /// Displays details of a specific Course by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the Course.</param>
+        /// <returns>A view containing the Course's details.</returns>
+        public IActionResult Show(int id)
+        {
+            
+            Course SelectedCourse = _api.FindCourse(id);
+            return View("~/Views/Course/Show.cshtml", SelectedCourse);
+        }
+    }
+}
---
+++ C1/Cumulative1/Cumulative1/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Cumulative1.Model
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Studentnumber { get; set; }
+        public DateTime? Enroldate { get; set; }
+    }
+}
